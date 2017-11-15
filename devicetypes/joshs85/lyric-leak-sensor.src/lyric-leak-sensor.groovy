922748a591f5b4d4c6deec33f18c9d935e806c00
Joshua Spain <joshs85@gmail.com>
2017-11-15 09:18:33 -0500

    Initial Device Handler Commit

---
+++ devicetypes/joshs85/lyric-leak-sensor.src/lyric-leak-sensor.groovy
@@ -0,0 +1,91 @@
+/**
+ *  Lyric Leak Sensor
+ *
+ *  Copyright 2017 Joshua Spain
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Lyric Leak Sensor", namespace: "joshs85", author: "Joshua Spain") {
+		capability "Water Sensor"
+		capability "Sensor"
+		capability "Battery"
+        capability "Temperature Measurement"
+        capability "Temperature Alarm"
+		capability "Health Check"
+	}
+    
+	tiles(scale: 2) {
+		multiAttributeTile(name:"water", type: "generic", width: 6, height: 4){
+			tileAttribute ("device.water", key: "PRIMARY_CONTROL") {
+				attributeState "dry", label: "Dry", icon:"st.alarm.water.dry", backgroundColor:"#ffffff"
+				attributeState "wet", label: "Wet", icon:"st.alarm.water.wet", backgroundColor:"#00A0DC"
+			}
+		}
+		standardTile("temperatureState", "device.temperature", width: 2, height: 2) {
+			state "normal", icon:"st.alarm.temperature.normal", backgroundColor:"#ffffff"
+			state "freezing", icon:"st.alarm.temperature.freeze", backgroundColor:"#00A0DC"
+			state "overheated", icon:"st.alarm.temperature.overheat", backgroundColor:"#e86d13"
+		}
+		valueTile("temperature", "device.temperature", width: 2, height: 2) {
+            state("temperature", label:'${currentValue}°',
+                backgroundColors:[
+                    [value: 31, color: "#153591"],
+                    [value: 44, color: "#1e9cbb"],
+                    [value: 59, color: "#90d2a7"],
+                    [value: 74, color: "#44b621"],
+                    [value: 84, color: "#f1d801"],
+                    [value: 95, color: "#d04e00"],
+                    [value: 96, color: "#bc2323"]
+                ]
+            )
+        }
+		valueTile("battery", "device.battery", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
+			state "battery", label:'${currentValue}% battery', unit:""
+		}
+        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", action:"refresh.refresh", icon:"st.secondary.refresh"
+        }
+		main (["water", "temperatureState"])
+		details(["water", "temperatureState", "temperature", "battery", "refresh"])
+}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'checkInterval' attribute
+	// TODO: handle 'DeviceWatch-DeviceStatus' attribute
+	// TODO: handle 'DeviceWatch-DeviceStatus' attribute
+	// TODO: handle 'temperatureAlarm' attribute
+	// TODO: handle 'water' attribute
+
+}
+
+def refresh() {
+	log.debug "refresh called"
+	poll()
+}
+
+void poll() {
+	log.debug "Executing 'poll' using parent SmartApp"
+	parent.pollChild()
+
+}
