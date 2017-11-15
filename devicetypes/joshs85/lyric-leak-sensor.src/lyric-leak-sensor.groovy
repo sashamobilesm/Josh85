6e2e173ec9963b4925e4a97bd3d9972e1bac797f
Joshua Spain <joshs85@gmail.com>
2017-11-15 09:20:29 -0500

    Corrected Bug

--- devicetypes/joshs85/lyric-leak-sensor.src/lyric-leak-sensor.groovy
+++ devicetypes/joshs85/lyric-leak-sensor.src/lyric-leak-sensor.groovy
@@ -62,10 +62,6 @@ metadata {
 	simulator {
 		// TODO: define status and reply messages here
 	}
-
-	tiles {
-		// TODO: define your main and details tiles here
-	}
 }
 
 // parse events into attributes
